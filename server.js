85547a6a179efa8b0aa68144b2c0807775d1d302
PravinK11 <pravinrk1105@gmail.com>
2026-03-14 18:36:18 +0530

    setup dotenv and environment variables

--- server.js
+++ server.js
@@ -1,29 +1,20 @@
-
 import express from 'express';
-import pool from './db.js'
+import dotenv from 'dotenv';
+
 const app=express();
-const port=8080;
+dotenv.config();
+
+
+const port = process.env.PORT || 8080;
+
 import  expenseRoutes from "./src/routes/expensesRoute.js"
 
+
+// const port=8080;
 
 app.use('/expenses', expenseRoutes);
 
 
-// app.get('/expense',async (req, res)=>{
-//     // res.send(`all expenses to show by user`);
-
-//     try {
-//         const [rows] = await pool.query('select * from expenses where user_id=5');
-//         if (!rows) {
-//             return res.status(400).json({ message: "expenses not found" })
-//         }
-//         return res.status(200).json(rows);
-//         res.send(rows);
-//     } catch (error) {
-//         return res.status(500).send(error)
-//     }
-
-// })
 
 app.listen(port,() =>{
     console.log(`App is litening to the port ${port}`)
