de03811e943e9dc7b33b0d0a0507edc2fa902437
PravinK11 <pravinrk1105@gmail.com>
2026-03-15 17:34:30 +0530

    Add POST API to create new expense

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-const app=express();
+const app = express();
 dotenv.config();
-
 
 const port = process.env.PORT || 8080;
 
-import  expenseRoutes from "./src/routes/expensesRoute.js"
+import expenseRoutes from "./src/routes/expensesRoute.js"
 
 
-// const port=8080;
+app.use(express.json());
 
 app.use('/expenses', expenseRoutes);
 
 
 
-app.listen(port,() =>{
+
+app.listen(port, () => {
     console.log(`App is litening to the port ${port}`)
 });
--- src/routes/expensesRoute.js
+++ src/routes/expensesRoute.js
@@ -1,8 +1,8 @@
 import express from 'express';
-const router= express.Router();
+const router = express.Router();
 import pool from "../../db.js";
 
-router.get('/',async (req, res)=>{
+router.get('/', async (req, res) => {
     try {
         const [rows] = await pool.query('select * from expenses where user_id=5');
         if (!rows) {
@@ -15,5 +15,24 @@ router.get('/',async (req, res)=>{
     }
 
 })
+
+router.post("/", async (req, res) => {
+    try {
+        console.log(req.body)
+        const { user_id,expense,expense_amount, expense_date,category_id } = req.body;
+        
+        if (!user_id || !expense || !expense_amount || !expense_date || !category_id) {
+            res.status(400).json({ message: "all fields required" })
+        }
+        const queryString = 'insert into expenses (user_id,expense,expense_amount, expense_date,category_id) values(?,?,?,?,?)';
+        const queryValues = [user_id,expense,expense_amount, expense_date,category_id];
+        const result = await pool.query(queryString, queryValues);
+        return res.status(200).json({ message: "expense added successfully" })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "internal server error" }, error)
+    }
+})
+
 
 export default router;
